d47aee8fbc504d7f1c4f7a092162e76e8e8b9adc
Juanita Velez <jvalentina.velez@gmail.com>
2024-05-24 07:05:24 -0500

    Incorporación de prueba para verificar renderización con props pasadas

--- src/tests/components/PostModal.test.jsx
+++ src/tests/components/PostModal.test.jsx
@@ -1,25 +1,32 @@
-import { render } from "@testing-library/react"
+import { fireEvent, screen, render } from "@testing-library/react"
 import { PostModal } from "../../components/PostModal"
 
 describe('Pruebas en <PostModal />', () => {
 
+    const mockHandleClose = jest.fn();
+    const mockHandleConfirmAction = jest.fn();
+
+    const postModalProps = {
+        open: true,
+        handleClose: mockHandleClose,
+        action: 'Edit Post',
+        postData: { userId: 1, id: 1, title: 'Testing Title', body: 'Testing Body' },
+        handleConfirmAction: mockHandleConfirmAction,
+        isDeleteSelected: false,
+    };
+
     test('should match the snapshot', () => {
-
-        const mockHandleClose = jest.fn();
-        const mockHandleConfirmAction = jest.fn();
-
-        const postModalProps = {
-            open: true,
-            handleClose: mockHandleClose,
-            action: 'Edit Post',
-            postData: { userId: 1, id: 1, title: 'Testing Title', body: 'Testing Body' },
-            handleConfirmAction: mockHandleConfirmAction,
-            isDeleteSelected: false,
-        };
 
         render(<PostModal {...postModalProps}/>)
 
-    })
+    });
+
+    test('should render correctly with given props', () => {
+        render(<PostModal {...postModalProps} />);
+    
+        expect(screen.getByLabelText(/Title/i)).toHaveValue('Testing Title');
+        expect(screen.getByLabelText(/Body/i)).toHaveValue('Testing Body');
+    });
 
 
 })
