8a60bbbe0800e4d09b99e4e6368fe706a3e55b1c
Juanita Velez <jvalentina.velez@gmail.com>
2024-05-24 07:08:14 -0500

    Adición de pruebas para botones de Confirmar y Cancelar

--- src/tests/components/PostModal.test.jsx
+++ src/tests/components/PostModal.test.jsx
@@ -10,15 +10,18 @@ describe('Pruebas en <PostModal />', () => {
         open: true,
         handleClose: mockHandleClose,
         action: 'Edit Post',
-        postData: { userId: 1, id: 1, title: 'Testing Title', body: 'Testing Body' },
+        postData: { 
+            userId: 1, 
+            id: 1, 
+            title: 'Testing Title', 
+            body: 'Testing Body' 
+        },
         handleConfirmAction: mockHandleConfirmAction,
         isDeleteSelected: false,
     };
 
     test('should match the snapshot', () => {
-
         render(<PostModal {...postModalProps}/>)
-
     });
 
     test('should render correctly with given props', () => {
@@ -26,6 +29,20 @@ describe('Pruebas en <PostModal />', () => {
     
         expect(screen.getByLabelText(/Title/i)).toHaveValue('Testing Title');
         expect(screen.getByLabelText(/Body/i)).toHaveValue('Testing Body');
+    });
+
+    test('should call handleClose when cancel button is clicked', () => {
+        render(<PostModal {...postModalProps} />);
+        
+        fireEvent.click(screen.getByText(/Cancel/i));
+        expect(mockHandleClose).toHaveBeenCalled();
+    });
+
+    test('should call handleConfirmAction when confirm button is clicked', () => {
+        render(<PostModal {...postModalProps} />);
+        
+        fireEvent.click(screen.getByText(/Confirm/i));
+        expect(mockHandleConfirmAction).toHaveBeenCalled();
     });
 
 
